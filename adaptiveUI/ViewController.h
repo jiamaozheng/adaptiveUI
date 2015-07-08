1c5cda3d2c14f7558777dc6b32105f415cf8d719
Jiamao Zheng <jiamao@emergemedia.com>
2015-07-08 12:34:47 -0500

    fssfdadfs

--- adaptiveUI/ViewController.h
+++ adaptiveUI/ViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by Jiamao Zheng on 7/8/15.
 //  Copyright (c) 2015 Emerge Media. All rights reserved.
-//
+// http://www.sitepoint.com/building-adaptive-user-interfaces-ios-8/
 
 #import <UIKit/UIKit.h>
 
