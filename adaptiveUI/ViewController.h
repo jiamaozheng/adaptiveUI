8fa9d2af8bab92f1f79c59d4d8bb50bb288118c9
Jiamao Zheng <jiamao@emergemedia.com>
2015-07-08 10:11:48 -0500

    Initial Commit

---
+++ adaptiveUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  adaptiveUI
+//
+//  Created by Jiamao Zheng on 7/8/15.
+//  Copyright (c) 2015 Emerge Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
